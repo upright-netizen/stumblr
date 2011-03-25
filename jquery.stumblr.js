0a0d28c68f3630040909d8af292078033ed41563
jared-stilwell <jared.k.stilwell@gmail.com>
2011-03-25 14:30:28 -0500

    Added some skeleton subscriber functions.

--- jquery.stumblr.js
+++ jquery.stumblr.js
@@ -2,7 +2,19 @@
 	var $host = null;
 	
 	$.fn.stumblr = function( url ) {
+		$host = $(this);
 		$.publish( "/stumblr/request", [ url ] );
 	};
 	
+	// Request the feed from the Tumblr API
+	$.subscribe( "/stumblr/request", function( url ){
+		// Make the AJAX request here using the url param.
+		// Publish the render event with the returned data.
+	});
+	
+	// Render the specified template for the content
+	$.subscribe( "/stumblr/render", function( data ){
+		// Render the template into the host element using
+		// 		the data param.
+	});
 })(jQuery);
