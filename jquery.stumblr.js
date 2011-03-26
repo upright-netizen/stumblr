dd6aae543c8361bd1b23bba0a7ec41a035d1731e
jared-stilwell <jared.k.stilwell@gmail.com>
2011-03-26 08:34:52 -0500

    Moved the URL suffix for the Tumblr API into the plugin. Keeps things a bit DRYer.

--- jquery.stumblr.js
+++ jquery.stumblr.js
@@ -1,11 +1,9 @@
 ;(function($){	
-	/*
-	*	Nathan Stilwell rules!!!
-	*/	
+
 	$.fn.stumblr = function( options ) {		
 		var elem = $(this);
 		var config = { dataType: 'jsonp' };
-		var a = $.ajax( options.url, config );
+		var a = $.ajax( options.url + '/api/read/json', config );
 		
 		a.success( function(data){
 			var content = $( options.template ).tmpl( data );
