78220cf495a550aeff06766a7bd5d89190282cfd
Nathan Stilwell <nathanstilwell@gmail.com>
2011-03-29 13:46:42 -0400

    making a mess trying to add this refresh

--- jquery.stumblr.js
+++ jquery.stumblr.js
@@ -7,18 +7,39 @@
 		
 		return this.each(function(){
 			var elem = $(this),
-				o = $.extend({}, opts, elem.data()),
-				a = $.ajax( o.url + '/api/read/json', o['ajax-data'] );
+				o = $.extend({}, opts, elem.data()), 
+				a = $.ajax( o.url + '/api/read/json', o['ajax-data'] ),
+				stumblr = {
+					elem : elem,
+					options : o,
+					ajax : a,
+					keepGoing : true,
+					refreshRate : o.refreshRate,
+					refresh : function(data){
+						
+						this.ajax.success(function(data){
+							//this.o.templateEngine($(this.o.template), data).appendTo(this.elem);
+							console.log('ping');
+						});
+						
+						if(this.keepGoing){
+							setTimeout(this.refresh, this.options.refreshRate);
+						}
+					}
+				};
+			
+			elem.data("stumblr", stumblr);
 			
 			if( o.templateType === undefined || o.templateType === 'tmpl' ){
-				o.templateEngine = $.tmpl;
+				elem.data().stumblr.options.templateEngine = $.tmpl;
 			}
 			
-			a.success( function(data){
-				//var content = $( o.template ).tmpl( data );
-				//elem.append( content );
-				o.templateEngine($(o.template), data).appendTo(elem);
-			});
+			elem.data().stumblr.refresh();
+			//a.success( function(data){
+			//	//var content = $( o.template ).tmpl( data );
+			//	//elem.append( content );
+			//	o.templateEngine($(o.template), data).appendTo(elem);
+			//});
 		});
 	};
 	//
@@ -39,7 +60,8 @@
 	$.fn.stumblr.defaults = {
 		url : "http://nathanstilwell.tumblr.com",
 		template : "#stumblr-posts-tmpl",
-		"ajax-data" : { dataType : 'jsonp' }
+		"ajax-data" : { dataType : 'jsonp' },
+		refreshRate : 500
 	};
 	
 })(jQuery, window, document);
