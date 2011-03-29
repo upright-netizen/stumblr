49dc341ac2f0047b581c7367e67b1ae523c9ff2b
jared-stilwell <jared.k.stilwell@gmail.com>
2011-03-28 23:19:26 -0500

    Added test cases for overriding default settings.

--- tests/units.js
+++ tests/units.js
@@ -1,3 +1,18 @@
+// Mock the Ajax requests
+var recordedAjaxUrl = "";
+var recordedAjaxOptions = {};
+
+jQuery.ajax = function( url, options ) {
+	recordedAjaxUrl = url;
+	recordedAjaxOptions = options;
+	
+	return {
+		success: function( callback ){ /* blank */ },
+		error: function( callback ){ /* blank */ }
+	};
+};
+
+// Unit Tests
 test('should have default config values', function() { 
 	ok( $.fn.stumblr, 'Plugin exists.' );
 	ok( $.fn.stumblr.defaults.url, 'Default URL is set' );	
@@ -6,4 +21,21 @@ test('should have default config values', function() {
 	ok( $.fn.stumblr.defaults["ajax-data"].dataType, 'Default Ajax data type is set' );
 	
 	equal( $.fn.stumblr.defaults["ajax-data"].dataType, 'jsonp', 'Ajax data type is JSONP by default' );
+});
+
+test('should override default settings by passing an object argument', function(){
+	var expected = "http://nowhere.org/no/place";
+	$('#test-area').stumblr({ url: expected });
+	equal( recordedAjaxUrl, expected + '/api/read/json', 'The URL was not properly overridden by object argument' );
+	
+	$('#test-area').stumblr({ 'ajax-data': 'test' });
+	equal( recordedAjaxOptions, 'test', 'The Ajax settings were not properly overridden by object argument' );
+});
+
+test('should override default ajax options based on data attributes of host element.', function() {
+	var expected = "http://somewhere.org/some/place";
+	$('#test-area').attr('data-url', expected);
+	$('#test-area').stumblr();
+	equal( recordedAjaxUrl, expected + '/api/read/json', 'The URL was not properly overridden by data attributes' );
+	
 });
